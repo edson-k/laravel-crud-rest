94b8cfe44a137ae81af094b348b16cf97164db5f
Edson Kuzniewski <contato@edsonk.com.br>
2019-04-08 15:25:35 -0300

    change config database free for test

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'ek_laravel_crud'),
+            'username' => env('DB_USERNAME', 'ek_laravel_crud'),
+            'password' => env('DB_PASSWORD', 'yEapG8HjGMejYLu'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
